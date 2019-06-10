f6fe843d85c1ce6a80fcb167af91d82a074bb050
Pierre Snell <pierre.snell.1@ulaval.ca>
2019-06-10 14:02:29 -0400

    working !!

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,8 @@
 from face_alignment import FaceAlignment, LandmarksType
 from torch.utils.data import DataLoader, Dataset
-from settings import ROOT_DATASET, BATCH_SIZE, DEVICE
+from settings import ROOT_DATASET, BATCH_SIZE, DEVICE, K_SHOT
 import glob
 import torch
-from torchvision import transforms
 
 import os
 import time
@@ -24,6 +23,8 @@ from torch.utils.data.dataset import random_split
 from torchvision import models, transforms
 from torch.optim.lr_scheduler import CosineAnnealingLR
 
+face_landmarks = FaceAlignment(LandmarksType._2D, device='cpu')
+
 
 def get_landmarks(image):
     fa = FaceAlignment(LandmarksType._3D,
@@ -38,32 +39,35 @@ def get_landmarks_folder(path_folder):
 
 
 class frameLoader(Dataset):
-    def __init__(self, root_dir=ROOT_DATASET, K_shots=8):
+    def __init__(self, root_dir=ROOT_DATASET, K_shots=K_SHOT):
         super(frameLoader, self).__init__()
-        self.face_landmarks = FaceAlignment(LandmarksType._2D,
-                                            flip_input=False,
-                                            device=str(DEVICE))
-        # device="cuda:0")
+        # self.face_landmarks = FaceAlignment(LandmarksType._2D, device='cpu')
         self.K_shots = K_shots
         self.root_dir = root_dir
         self.ids = glob.glob(f"{self.root_dir}/*")
         self.contexts = glob.glob(f"{self.root_dir}/*/*")
         self.mp4files = glob.glob(f"{self.root_dir}/*/*/*")
 
-        self.id_to_int = {name.split('/')[-1]: torch.tensor([i])
-                          for i, name in enumerate(self.ids)}
+        self.id_to_tensor = {name.split('/')[-1]: torch.tensor(i).view(1)
+                             for i, name in enumerate(self.ids)}
 
     def __getitem__(self, index):
+        # torch.set_grad_enabled(False)
         mp4File = self.mp4files[index]
-        itemId = self.id_to_int[mp4File.split('/')[-3]]
-        # print(mp4File)
+        itemId = self.id_to_tensor[mp4File.split('/')[-3]]
         video = cv2.VideoCapture(mp4File)
         total_frame_nb = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
         frameIndex = np.random.randint(0, total_frame_nb)
         video.set(cv2.CAP_PROP_POS_FRAMES, frameIndex)
-        gt_im = video.read()[1]
+        _, gt_im = video.read()
+        gt_im = cv2.cvtColor(gt_im, cv2.COLOR_BGR2RGB)
+
+        landmarks = face_landmarks.get_landmarks(gt_im)
+        landmarks = landmarks[0]
+        # landmarks = np.random.rand(68, 2)
         landmark_image = np.zeros(gt_im.shape, np.float32)
-        landmarks = self.face_landmarks.get_landmarks(gt_im)[0]
+        #  gt_im=np.array(gt_im, np.float32).transpose(2, 1, 0)
+        gt_im_tensor = transforms.ToTensor()(gt_im)
         # Machoire
         cv2.polylines(landmark_image, [np.int32(landmarks[0:17])],
                       isClosed=False, color=(0, 255, 0))
@@ -92,83 +96,78 @@ class frameLoader(Dataset):
         cv2.polylines(landmark_image, [np.int32(landmarks[60:68])],
                       isClosed=True, color=(255, 255, 0))
 
+        #  gt_landmarks = torch.tensor(landmark_image.transpose(2, 1, 0))
         gt_landmarks = transforms.ToTensor()(landmark_image)
-        gt_im_tensor = transforms.ToTensor()(gt_im)
-        # print("gt_done")
         indexOk = False
         contextIndex = (frameIndex+(total_frame_nb//10)) % total_frame_nb
         while not indexOk:
             if contextIndex + self.K_shots > total_frame_nb:
                 contextIndex -= 1
-                # print(f"reduce ! frameIndex is {frameIndex} :",
-                #      f"contextIndex was {contextIndex} on {total_frame_nb}")
             else:
                 indexOk = True
 
         video.set(cv2.CAP_PROP_POS_FRAMES, contextIndex)
-        context_tensors = []
-        for i in range(self.K_shots):
-            contextFrame = video.read()[1]
-            ctxt_ldmk_img = np.zeros(contextFrame.shape, np.float32)
-            ctxt_landmarks = self.face_landmarks.get_landmarks(contextFrame)[0]
-            context_tensors.append(transforms.ToTensor()(contextFrame))
-
+        context_tensors_list = []
+        for _ in range(self.K_shots):
+            _, contextFrame = video.read()
+            contextFrame = cv2.cvtColor(contextFrame, cv2.COLOR_BGR2RGB)
+            # ctxt_landmarks = self.face_landmarks.get_landmarks(contextFrame)[0]
+            ctxt_landmarks = np.random.rand(68, 2)
             # Machoire
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[0:17])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[0:17])],
                           isClosed=False, color=(0, 255, 0))
             # Sourcil Gauche
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[17:22])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[17:22])],
                           isClosed=False, color=(255, 0, 0))
             # Sourcil droit
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[22:27])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[22:27])],
                           isClosed=False, color=(255, 0, 0))
             # Nez arrete
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[27:31])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[27:31])],
                           isClosed=False, color=(255, 0, 255))
             # Nez narine
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[31:36])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[31:36])],
                           isClosed=False, color=(255, 0, 255))
             # Oeil gauche
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[36:42])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[36:42])],
                           isClosed=True, color=(0, 0, 255))
             # oeil droit
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[42:48])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[42:48])],
                           isClosed=True, color=(0, 0, 255))
             # Bouche exterieur
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[48:60])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[48:60])],
                           isClosed=True, color=(255, 255, 0))
             # Bouche interieur
-            cv2.polylines(ctxt_ldmk_img, [np.int32(ctxt_landmarks[60:68])],
+            cv2.polylines(contextFrame, [np.int32(ctxt_landmarks[60:68])],
                           isClosed=True, color=(255, 255, 0))
-            context_tensors.append(transforms.ToTensor()(ctxt_ldmk_img))
-        # print("context done")
-        context_tensors = torch.cat(context_tensors)
+            # contextFrame=np.array(contextFrame,np.float32).transpose(2, 1, 0)
+            # context_tensors_list.append(torch.tensor(contextFrame))
+            context_tensors_list.append(transforms.ToTensor()(contextFrame))
+
+            # context_tensors=torch.rand(self.K_shots, 224, 224)
+            # torch.cat(context_tensors_list, out=context_tensors)
+            context_tensors = torch.cat(context_tensors_list)
+        video.release()
         return gt_im_tensor, gt_landmarks, context_tensors, itemId
 
     def __len__(self):
         return len(self.mp4files)
 
 
-def get_data_loader(root_dir=ROOT_DATASET, K_shots=8):
-    datas = frameLoader(root_dir=ROOT_DATASET, K_shots=8)
+def get_data_loader(root_dir=ROOT_DATASET, K_shots=8, workers=0):
+    datas = frameLoader(root_dir=root_dir, K_shots=K_shots)
     # print(len(datas))
     # size_train = int(0.8 * len(datas))
     # size_valid = len(datas) - int(0.8 * len(datas))
     # train_datas, valid_datas = random_split(datas, (size_train, size_valid))
-    train_loader = DataLoader(datas, batch_size=BATCH_SIZE,
-                              shuffle=True, num_workers=8)
+    # pin = False if DEVICE.type == 'cpu' else True
+    train_loader = DataLoader(datas, batch_size=BATCH_SIZE, shuffle=True)
+    # num_workers=workers, pin_memory=pin)
     return train_loader, len(datas.ids)
 
 
-def view_batch():
-    datas = frameLoader()
-    # print(len(datas))
-    # size_train = int(0.8 * len(datas))
-    # size_valid = len(datas) - int(0.8 * len(datas))
-    # train_datas, valid_datas = random_split(datas, (size_train, size_valid))
-    train_loader = DataLoader(datas, batch_size=BATCH_SIZE,
-                              shuffle=True, num_workers=8)
-    real_batch = next(train_loader)
+def view_batch(loader):
+    real_batch = next(loader)
     batch_view = torch.stack(real_batch[0], dim=1).view(-1, 3, 224, 224)
     # %matplotlib inline
     plt.figure(figsize=(25, 10))
--- test.py
+++ test.py
@@ -1,63 +1,129 @@
-from settings import NB_EPOCHS, LEARNING_RATE
-from preprocess import get_data_loader
-from archi import Embedder, Generator, Discriminator
-from losses import adverserialLoss, matchLoss, contentLoss, discriminatorLoss
-from torch.optim import Adam
+
+
+import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
+import numpy as np
 import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+
+from archi import Discriminator, Embedder, Generator
+from losses import adverserialLoss, contentLoss, discriminatorLoss, matchLoss
+from preprocess import get_data_loader, frameLoader
+from settings import BATCH_SIZE, DEVICE, K_SHOT, LEARNING_RATE, NB_EPOCHS
+from utils import (Checkpoints, load_models, plot_grad_flow, get_size)
+
+mplstyle.use(['dark_background', 'fast'])
+
 # ######
 # Init #
 # ######
-train_loader, nb_pers = get_data_loader()
-emb = Embedder()
-gen = Generator()
-disc = Discriminator(nb_pers)
+
+
+class testLoader(Dataset):
+
+    def __init__(self):
+        super(testLoader, self).__init__()
+        self.plop = 3
+
+    def __getitem__(self, index):
+        a = torch.rand(3, 224, 224)
+        b = torch.rand(3, 224, 224)
+        c = torch.rand(K_SHOT*3, 224, 224)
+        d = torch.randint(0, 118, (1,))
+        return a, b, c, d
+
+    def __len__(self):
+        return 36237
+
+
+datas = frameLoader()
+train_loader = DataLoader(datas, batch_size=BATCH_SIZE,
+                          shuffle=True, num_workers=0)
+nb_pers = 118
+
+# train_loader, nb_pers = get_data_loader()
+
+emb, gen, disc = load_models(nb_pers, load_previous_state=False)
+print("Nombre de paramètres Emb: ",
+      f"{sum([np.prod(p.size()) for p in emb.parameters()]):,}")
+
+print("Nombre de paramètres Gen: ",
+      f"{sum([np.prod(p.size()) for p in gen.parameters()]):,}")
+
+print("Nombre de paramètres Disc: ",
+      f"{sum([np.prod(p.size()) for p in disc.parameters()]):,}")
+
+
 advLoss = adverserialLoss()
 mchLoss = matchLoss()
 cntLoss = contentLoss()
 dscLoss = discriminatorLoss()
+
+check = Checkpoints()
 optimizerEmb = Adam(emb.parameters(), lr=LEARNING_RATE)
 optimizerGen = Adam(gen.parameters(), lr=LEARNING_RATE)
 optimizerDisc = Adam(disc.parameters(), lr=LEARNING_RATE)
+
+plt.ion()
+fig, axes = plt.subplots(3, 3, figsize=(15, 10), num='Mon')
+# ##########
+# Training #
+# ##########
+print("torch version : ", torch.__version__)
+# torch.autograd.set_detect_anomaly(True)
 for i_epoch in range(NB_EPOCHS):
     for i_batch, batch in enumerate(train_loader):
-        # #############
         # Passe avant #
-        # #############
+        optimizerEmb.zero_grad()
+        optimizerDisc.zero_grad()
+        optimizerGen.zero_grad()
         print("test : ", i_epoch, i_batch)
-        gt_im, gt_landmarks, context_tensors, itemIds = batch
-        # print("itemIds", itemIds)
-        # print("Gt_imsize", gt_im.size())
-        out_emb, out_paramIn = emb(context_tensors)
-        # print("emb size", out_emb.size())
-        out_gen = gen(gt_landmarks, out_paramIn)
-        out_gen = out_gen[:, :, 0:224, 0:224]
-        # print("Gen size", out_gen.size())
-        score_synth, feature_maps_synth = disc(torch.cat((out_gen,
-                                                          gt_landmarks),
-                                                         dim=1),
-                                               itemIds)
-        # print("Disc synth ok")
-        score_gt, feature_maps_gt = disc(torch.cat((gt_im, gt_landmarks),
-                                                   dim=1), itemIds)
+        gt_im, gt_landmarks, context, itemIds = batch
+        gt_im.to(DEVICE)
+        gt_landmarks.to(DEVICE)
+        context.to(DEVICE)
+        itemIds.to(DEVICE)
+        print(gt_im.size())
+        print(gt_landmarks.size())
+        print(context.size())
+        print(itemIds.size())
+        print(gt_im.type(), gt_im.requires_grad)
+        print(gt_landmarks.type(), gt_landmarks.requires_grad)
+        print(context.type(), context.requires_grad)
+        print(itemIds.type(), itemIds.requires_grad)
+        embeddings = emb(context)
+        # print("1  ", embeddings.requires_grad)
+
+        synth_im = gen(gt_landmarks)
+        score_synth, feature_maps_disc_synth = disc(torch.cat((synth_im,
+                                                               gt_landmarks),
+                                                              dim=1), itemIds)
+        score_gt, feature_maps_disc_gt = disc(torch.cat((gt_im, gt_landmarks),
+                                                        dim=1), itemIds)
         # #################
         # Calcul des loss #
         # #################
-        print(score_synth)
-        lossAdv = advLoss(score_synth, feature_maps_synth,
-                          feature_maps_gt)
-        # print("Adv loss ok ")
-        lossCnt = cntLoss(gt_im, out_gen)
-        # print("Cnt loss ok ")
-        lossMch = mchLoss(out_emb, disc.embeddings(itemIds))
-        # print("Mch loss ok ")
+        lossAdv = advLoss(score_synth, feature_maps_disc_gt,
+                          feature_maps_disc_synth)
+        lossCnt = cntLoss(gt_im, synth_im)
+        lossMch = mchLoss(embeddings, disc.embeddings(itemIds))
         lossDsc = dscLoss(score_gt, score_synth)
-        print("Dsc loss ok ")
-        print("lossAdv",  lossAdv)
-        print("lossCnt",  lossCnt)
-        print("lossMch",  lossMch)
-        print("lossDsc",  lossDsc)
+        print("lossAdv",  lossAdv.data)
+        print("lossCnt",  lossCnt.data)
+        print("lossMch",  lossMch.data)
+        print("lossDsc",  lossDsc.data, score_gt.data, score_synth.data)
         loss = lossAdv + lossCnt + lossMch + lossDsc
+        print("LOSS  : ", loss.requires_grad)
         loss.backward()
         optimizerEmb.step()
         optimizerDisc.step()
         optimizerGen.step()
+
+        check.addCheckpoint(lossAdv + lossCnt + lossMch, lossDsc)
+        check.save(lossAdv + lossCnt + lossMch, lossDsc, emb, gen, disc)
+        if i_batch % 10 == 0:
+            check.visualize(fig, axes,
+                            gt_landmarks.detach()[0], synth_im.detach()[0],
+                            gt_im.detach()[0], emb, gen, disc)
