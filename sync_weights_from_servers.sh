f8d2bcc3d3e7e0895b8559af0548abe00b1c0dbc
Pierre Snell <pierre.snell.1@ulaval.ca>
2019-10-08 22:39:22 -0400

    scripts double ok

--- sync_weights_from_servers.sh
+++ sync_weights_from_servers.sh
@@ -2,7 +2,11 @@
 echo "-_-_-_-_- Rapatriement des poids de Helios -_-_-_-_-"
 rsync -zav "$1@helios.calculquebec.ca:PapierFewShot/weights/" "./weights"
 echo "-_-_-_-_-_-_-_-_-_-_-   DONE   -_-_-_-_-_-_-_-_-_-_-"
+echo ""
+echo ""
 
 echo "-_-_-_-_- Rapatriement des poids de Beluga -_-_-_-_-"
 rsync -zav "$1@beluga.calculcanada.ca:PapierFewShot/weights/" "./weights"
 echo "-_-_-_-_-_-_-_-_-_-_-   DONE   -_-_-_-_-_-_-_-_-_-_-"
+echo ""
+echo ""
