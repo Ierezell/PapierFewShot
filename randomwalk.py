109945b9fc3c40d17470b7535fb2a403316f2138
Pierre Snell <pierre.snell.1@ulaval.ca>
2020-01-21 15:24:45 -0500

    before antoine

--- randomwalk.py
+++ randomwalk.py
@@ -1,8 +1,10 @@
 import torch
+import torchvision
 from models_ldmk import Generator
 from settings import DEVICE, BATCH_SIZE
-
+import matplotlib as plt
 FACTOR = 30
+NB_IMAGE = 20
 
 if __name__ == '__main__':
 
@@ -21,12 +23,13 @@ if __name__ == '__main__':
         print("Weights not found")
 
     input_noise = torch.randn(BATCH_SIZE, 10, 1, 1, device=DEVICE)
-    input_noise.flatten()
 
-    random_walk = torch.randn(len(input_noise)) / FACTOR
+    for _ in range(NB_IMAGE):
+        value = torch.randn(BATCH_SIZE, input_noise.size(1)) / FACTOR
+        input_noise += value
+        img = gen(input_noise)
+        img_list.append(img)
 
-    for i, value in enumerate(random_walk):
-        input_noise[i] += value
-        img = gen(input_noise.view(BATCH_SIZE, 10, 1, 1))
-        if i % 10:
-            img_list.append(img)
+    grid = torchvision.utils.make_grid(torch.stack(img_list), padding=4, nrow=2,
+                                       normalize=True, scale_each=True)
+    plt.imshow(grid.permute(1, 2, 0))
