f58dc60b283a68ac6c05b958a8dbef06dcb6f8e4
Antoine Prieur <antoine.prieur45@gmail.com>
2020-01-22 14:36:23 -0500

    yut

--- randomwalk.py
+++ randomwalk.py
@@ -1,20 +1,27 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import torch
 import torchvision
-from models_ldmk import Generator
-from settings import DEVICE, BATCH_SIZE
-FACTOR = 30
-NB_IMAGE = 20
+import torchvision.transforms as transforms
+import numpy as np
+from train_ldmk2 import Generator
+from settings import DEVICE, BATCH_SIZE_LDMK
+from preprocess import write_landmarks_on_image
+plt.ion()
+
+FACTOR = 2
+NB_IMAGE = 100
 
 if __name__ == '__main__':
 
     img_list = []
 
     gen = Generator()
+    gen.eval()
+
     gen = gen.to(DEVICE)
 
     try:
-        gen.load_state_dict(torch.load("./weights/ldmk/gen.pt",
+        gen.load_state_dict(torch.load("./weights/ldmk/generator_224.pt",
                                        map_location=DEVICE))
     except RuntimeError:
         gen.load_state_dict(torch.load("./weights/ldmk/gen.bk",
@@ -22,14 +29,33 @@ if __name__ == '__main__':
     except FileNotFoundError:
         print("Weights not found")
 
-    input_noise = torch.randn(BATCH_SIZE, 10, 1, 1, device=DEVICE)
+    input_noise = torch.randn(BATCH_SIZE_LDMK, 100, device=DEVICE)
+
+    # input_noise = Variable(Tensor(np.random.normal(0, 1, (ldmks.shape[0], 100))))
 
     for _ in range(NB_IMAGE):
-        value = torch.randn(BATCH_SIZE, input_noise.size(1)) / FACTOR
+        value = torch.randn(
+            BATCH_SIZE_LDMK, input_noise.size(1), device=DEVICE) / FACTOR
         input_noise += value
+        print(input_noise.size())
         img = gen(input_noise)
-        img_list.append(img)
 
-    grid = torchvision.utils.make_grid(torch.stack(img_list), padding=4, nrow=2,
-                                       normalize=True, scale_each=True)
-    plt.imshow(grid.permute(1, 2, 0))
+        gen_ldmk_im = []
+        for k in range(img.size(0)):
+            genli = np.zeros((224, 224, 3), np.float32)
+
+            l_gen = img[k]*224
+
+            genli = write_landmarks_on_image(genli, l_gen.int().cpu())
+            genli = transforms.ToTensor()(genli)
+
+            gen_ldmk_im.append(genli)
+
+        gen_ldmk_im = torch.stack(gen_ldmk_im)
+        img_list.append(gen_ldmk_im)
+
+        print(torch.stack(img_list).size())
+        grid = torchvision.utils.make_grid(torch.stack(img_list).view(-1, 3, 224, 224), padding=4, nrow=25,
+                                           normalize=True, scale_each=True)
+        plt.imshow(grid.permute(1, 2, 0))
+        plt.draw()
